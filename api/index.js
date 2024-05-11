960712544e0fcfe0c4b7f5d213752efeae90232b
Varshini-47 <pandiri_b221096cs@nitc.ac.in>
2024-05-12 00:16:52 +0530

    create signup api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
+import userRoutes from './routes/user.route.js';
+import authRoutes from './routes/authroute.js';
 dotenv.config();
 mongoose.connect(process.env.MONGO).then(
     ()=>{
@@ -12,6 +14,10 @@ mongoose.connect(process.env.MONGO).then(
     }
 );
 const app=express();
+app.use(express.json());
 app.listen(3000,()=>{
     console.log('Server is running on port 3000!');
 });
+
+app.use('/api/user',userRoutes);
+app.use('/api/auth',authRoutes);

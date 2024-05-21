98a8520e504e5e71b6b1e92a082561fa9bcac114
varshini-47 <varshini.pandiri04@gmail.com>
2024-05-22 00:37:27 +0530

    add delete post functionality

--- client/src/components/DashPosts.jsx
+++ client/src/components/DashPosts.jsx
@@ -1,14 +1,17 @@
-import { Table } from 'flowbite-react';
+import { Modal, Table } from 'flowbite-react';
 import React, { useEffect, useState } from 'react'
+import { HiOutlineExclamationCircle } from 'react-icons/hi';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { Button } from 'flowbite-react';
 /*we cant use an async function inside an useeffect*/
 
 export default function DashPosts() {
     const {currentUser}=useSelector((state)=>state.user);
     const [userPosts,setUserPosts]=useState([]);
     const [showMore,setShowMore]=useState(true);
-    console.log(userPosts)
+    const [showModal,setshowModal]=useState(false);
+    const [postIdToDelete,setPostIdToDelete]=useState('');
     useEffect(()=>{
         const fetchPosts=async ()=>{
             try{
@@ -39,13 +42,37 @@ export default function DashPosts() {
                 {
                     setUserPosts((prev)=>[...prev,...data.posts]);
                     if(data.posts.length<9)
+                        {
                         setShowMore(false);
+                        }
                 }
         }catch(error)
         {
             console.log(error.message);
         }
     }
+
+    const handleDeletePost=async()=>{
+        setshowModal(false);
+        try {
+            const res= await fetch(`/api/post/deletepost/${postIdToDelete}/${currentUser._id}`,{
+                method:'DELETE',
+            });
+            const data=await res.json();
+            if(!res.ok)
+                {
+                    console.log(data.message);
+                }
+                else{
+                    setUserPosts((prev)=>
+                        prev.filter((post)=>post._id!==postIdToDelete)
+                    );
+                }
+
+        } catch (error) {
+            console.log(error.message);
+        }
+    };
   return (
     <div className='table-auto w-full overflow-x-scroll md:mx-auto p-3 scrollbar scrollbar-track-slate-100 scrollbar-thumb-slate-300 dark:scrollbar-track-slate-700 scrollbar-thumb-slate-500'>
       {currentUser.isAdmin && userPosts.length>0?(<>
@@ -67,7 +94,10 @@ export default function DashPosts() {
                         <Table.Cell><Link className='font-medium text-gray-900 dark:text-white' to={`/post/${post.slug}`}>{post.title}</Link></Table.Cell>
                         <Table.Cell>{post.category}</Table.Cell>
                         <Table.Cell>
-                            <span className='font-medium text-red-500 hover:underline cursor-pointer'>Delete</span>
+                            <span onClick={()=>{
+                                setshowModal(true);
+                                setPostIdToDelete(post._id);
+                            }}className='font-medium text-red-500 hover:underline cursor-pointer'>Delete</span>
                         </Table.Cell>
                         <Table.Cell>
                             <Link className='text-teal-500 hover:underline' to={`/update-post/${post._id}`}>
@@ -82,6 +112,19 @@ export default function DashPosts() {
                 <button className='w-full text-teal-500 self-center text-sm py-7' onClick={handleShowmore}>Show more</button>
             )}
       </>):(<p>You have no posts yet</p>)}
+      <Modal show={showModal} onClose={()=>setshowModal(false)} popup size='md'><Modal.Header/>
+      <Modal.Body>
+        <div className='text-center'>
+          <HiOutlineExclamationCircle className='h-14 w-14 text-gray-400 dark:text-gray-200 mb-4 mx-auto'/>
+          <h3 className='mb-5 text-lg text-gray-500 daek:text-gray-400'>Are you sure you want to delete your account?</h3>
+          <div className='flex justify-center gap-4'>
+            <Button color='failure' onClick={handleDeletePost}>Yes,I'm sure</Button>
+            <Button color='gray' onClick={()=>setshowModal(false)}>No,Cancel</Button>
+          </div>
+        </div>
+      </Modal.Body>
+      
+      </Modal>
     </div>
   )
 }

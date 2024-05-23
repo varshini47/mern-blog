e547ef18c8b2234fddd64465722b122d65a3f4e2
varshini-47 <varshini.pandiri04@gmail.com>
2024-05-23 17:11:52 +0530

    Add comments in post page

--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -3,6 +3,7 @@ import React, { useState } from 'react'
 import { useEffect } from 'react'
 import { Link, useParams } from 'react-router-dom'
 import CallToAction from '../components/CallToAction';
+import CommentSection from '../components/CommentSection';
 export default function PostPage() {
 
     const {postSlug}=useParams();
@@ -51,6 +52,7 @@ export default function PostPage() {
       </div>
       <div  className='p-3 max-w-2xl mx-auto w-full post-content' dangerouslySetInnerHTML={{__html:post && post.content}}></div>
       <div className='max-w-4xl mx-auto w-full'><CallToAction/></div>
+      <CommentSection postId={post._id}/>
     </main>
   )
 }

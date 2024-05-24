ff8c17d0586e79b8d2e30703056b4e835238497d
varshini-47 <varshini.pandiri04@gmail.com>
2024-05-24 22:27:51 +0530

    add delete comment functionality

--- client/src/components/Comment.jsx
+++ client/src/components/Comment.jsx
@@ -3,7 +3,7 @@ import moment from 'moment';
 import {FaThumbsUp} from 'react-icons/fa'
 import { useSelector } from 'react-redux';
 import {Textarea,Button} from 'flowbite-react';
-export default function Comment({comment,onLike,onEdit}) {
+export default function Comment({comment,onLike,onEdit,onDelete}) {
     const [user,setUser]=useState({});
     const [isEditing,setIsEditing]=useState(false);
     const [editedContent,setEditedContent]=useState(comment.content);
@@ -12,7 +12,6 @@ export default function Comment({comment,onLike,onEdit}) {
         const getUser = async ()=>{
             try{
                 const res= await fetch(`/api/user/${comment.userId}`);
-                console.log(res);
                 const data=await res.json();
                
                 if(res.ok)
@@ -46,7 +45,7 @@ export default function Comment({comment,onLike,onEdit}) {
         if(res.ok)
           {
             setIsEditing(false);
-            onEdit(comment,editedContent);
+            onEdit(comment,editedContent);  
           }
       } catch (error) {
         console.log(error.message);
@@ -89,7 +88,10 @@ export default function Comment({comment,onLike,onEdit}) {
               </p>
               {
                 currentUser && (currentUser._id===comment.userId || currentUser.isAdmin ) && (
+                  <>
                   <button type='button' onClick={handleEdit} className='text-gray-400 hover:text-blue-500'>Edit</button>
+                  <button type='button' onClick={()=>onDelete(comment._id)} className='text-gray-400 hover:text-red-500'>Delete</button>
+                  </>
                 )
               }
             </div>

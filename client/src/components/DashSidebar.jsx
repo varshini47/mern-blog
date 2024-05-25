03e756a9de8d54c9c1c61ba26fca241e394d8499
varshini-47 <varshini.pandiri04@gmail.com>
2024-05-25 23:57:19 +0530

    add dashboard overview to admin dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -1,6 +1,6 @@
-import { Sidebar } from 'flowbite-react'
+import { Sidebar, SidebarItem } from 'flowbite-react'
 import React from 'react'
-import {HiAnnotation, HiArrowSmRight, HiDocumentText, HiOutlineUserGroup, HiUser} from 'react-icons/hi'
+import {HiAnnotation, HiArrowSmRight, HiChartPie, HiDocumentText, HiOutlineUserGroup, HiUser} from 'react-icons/hi'
 import { useEffect,useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { Link } from 'react-router-dom';
@@ -42,6 +42,13 @@ export default function DashSidebar() {
     <Sidebar className='w-full md:w-56'>
       <Sidebar.Items>
         <Sidebar.ItemGroup className='flex flex-col gap-1'>
+        {
+          currentUser && currentUser.isAdmin && (
+            <Link to='/dashboard?tab=dash'>
+              <Sidebar.Item active={tab==='dash' || !tab} icon={HiChartPie} as='div'>Dashboard</Sidebar.Item>
+            </Link>
+          )
+        }
         <Link to='/dashboard?tab=profile'>
             <Sidebar.Item active={tab==='profile'} icon={HiUser} label={currentUser.isAdmin?'Admin':'User'} labelColor='dark' as='div'>
               Profile
@@ -60,12 +67,7 @@ export default function DashSidebar() {
           <Sidebar.Item active={tab==='comments'} icon={HiAnnotation} as='div' >Comments</Sidebar.Item> {/*as div is used because of sidebar an anchor tag is there and in link one anchor tag is there which is causing an error*/}
         </Link>
         </>
-        
         }
-
-    
-
-        
             <Sidebar.Item icon={HiArrowSmRight} className='cursor-pointer' onClick={handleSignout}>
               Sign Out
             </Sidebar.Item>

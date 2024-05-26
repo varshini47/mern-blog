a96380504e25ea65dc9bff71b356f9d2d08709bb
varshini-47 <varshini.pandiri04@gmail.com>
2024-05-26 23:22:20 +0530

    Complete about and projects page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -2,8 +2,25 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>
-      About
+    <div className='min-h-screen flex items-center justify-center'>
+      <div className='max-w-2xl mx-auto p-3 text-center'>
+        <div className=''>
+          <h1 className='text-3xl font-semibold text text-center my-7'>About Varshini's Blog</h1>
+          <div className='text-md text-gray-500 flex flex-col gap-6'>
+            <p>
+              Varshini's Blog is a blog that i created while learning web development I have used both frontend and backened to create it
+            </p>
+
+            <p>
+              Varshini's Blog is a blog that i created while learning web development I have used both frontend and backened to create it
+            </p>
+
+            <p>
+              Varshini's Blog is a blog that i created while learning web development I have used both frontend and backened to create it
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-
+import CallToAction from '../components/CallToAction'
 export default function Projects() {
   return (
-    <div>
-    Projects
+    <div className='min-h-screen mx-auto max-w-2xl flex justify-center items-center flex-col p-3 gap-6'>
+    <h1 className='text-3xl font-semibold'>Projects</h1>
+    <p className='text-md text-gray-500 '>Build fun and engaging projects while learning HTML,CSS and JavaScript</p>
+    <CallToAction/>
     </div>
   )
 }
